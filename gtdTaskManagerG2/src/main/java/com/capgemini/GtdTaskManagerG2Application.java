8aee2cf223320407d0a99136177a8fa5332cfa69
andrefer <andrefer@les007596.corp.capgemini.com>
2021-08-10 14:12:44 +0200

    Creación del proyecto

---
+++ gtdTaskManagerG2/src/main/java/com/capgemini/GtdTaskManagerG2Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdTaskManagerG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdTaskManagerG2Application.class, args);
+	}
+
+}
---
+++ gtdTaskManagerG2/src/test/java/GTD/Task/Manager/Ampliaci/n/GtdTaskManagerG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package GTD.Task.Manager.Ampliaci.n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtdTaskManagerG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
