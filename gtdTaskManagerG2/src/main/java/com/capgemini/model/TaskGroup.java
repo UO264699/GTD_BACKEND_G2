4dc23a037b3e2ebdff2f336e089d8fba640f4401
Garcia Zapico <enol.garcia-zapico@capgemini.com>
2021-08-11 18:25:07 +0200

    Added method in TaskGroup to add task and reference it in task

--- gtdTaskManagerG2/src/main/java/com/capgemini/model/TaskGroup.java
+++ gtdTaskManagerG2/src/main/java/com/capgemini/model/TaskGroup.java
@@ -1,6 +1,7 @@
 package com.capgemini.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -43,7 +44,14 @@ public class TaskGroup {
 		this.name = name;
 	}
 	
-	
+	public void addTask(Task task) {
+		if(tasks == null) {
+			tasks = new ArrayList<>();
+		}
+		
+		tasks.add(task);
+		task.setTaskGroup(this);
+	}
 	
 
 }
