3782c152e73444bbf91cce39712aed1e1f32ba9e
andrefer <andrefer@les007596.corp.capgemini.com>
2021-08-11 12:40:00 +0200

    Task Group entity, repository, service and controller

--- gtdTaskManagerG2/src/main/java/com/capgemini/services/TaskController.java
+++ gtdTaskManagerG2/src/main/java/com/capgemini/services/TaskController.java
@@ -1,4 +1,4 @@
-package com.capgemini.controllers;
+package com.capgemini.services;
 
 import java.util.Date;
 import java.util.List;
---
+++ gtdTaskManagerG2/src/main/java/com/capgemini/services/implementation/TaskGroupService.java
@@ -0,0 +1,43 @@
+package com.capgemini.services.implementation;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.capgemini.model.TaskGroup;
+
+public interface TaskGroupService {
+
+	/**
+	 * @param <S>
+	 * @param entity
+	 * @return
+	 * @see org.springframework.data.repository.CrudRepository#save(java.lang.Object)
+	 */
+	<S extends TaskGroup> S save(S entity);
+
+	/**
+	 * @return
+	 * @see org.springframework.data.jpa.repository.JpaRepository#findAll()
+	 */
+	List<TaskGroup> findAll();
+
+	/**
+	 * @param id
+	 * @return
+	 * @see org.springframework.data.repository.CrudRepository#findById(java.lang.Object)
+	 */
+	Optional<TaskGroup> findById(Long id);
+
+	/**
+	 * @param entity
+	 * @see org.springframework.data.repository.CrudRepository#delete(java.lang.Object)
+	 */
+	void delete(TaskGroup entity);
+
+	/**
+	 * 
+	 * @see org.springframework.data.repository.CrudRepository#deleteAll()
+	 */
+	void deleteAll();
+
+}
